504f82a680a3d37c5a99d6a75731ae36abb82be2
appsplash99 <apurvchimralwar1997@gmail.com>
2021-02-23 17:50:19 +0530

    refactor: uncommented all working code

--- build-logic-solutions/001_operators_branching_loops/easy..js
+++ build-logic-solutions/001_operators_branching_loops/easy..js
@@ -1,57 +1,57 @@
 /* 1.  
 Write a program to add 5 numbers. 
 The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.*/
-// const addFiveNums = (a, b, c, d, e) => {
-//   return a + b + c + d + e
-// }
-// console.log(addFiveNums(1, 2, 3, 4, 5))
+const addFiveNums = (a, b, c, d, e) => {
+  return a + b + c + d + e
+}
+console.log(addFiveNums(1, 2, 3, 4, 5))
 
 
 /* 2.  
 Write a program to take a number input from user and 
 determine whether the number is odd or even.*/
-// let readlineSync = require('readline-sync'); 
-// let userNum = readlineSync.question('Enter a Number: ');
-// console.log(`Entered number: ${userNum} is ${userNum%2!==0 ? 'Odd': 'Even'}`);
+let readlineSync = require('readline-sync'); 
+let userNum = readlineSync.question('Enter a Number: ');
+console.log(`Entered number: ${userNum} is ${userNum%2!==0 ? 'Odd': 'Even'}`);
 
 /* 3.
 Write a program to find the maximum and minimum out of two given numbers. 
 The numbers are num1=129 and num2=251.*/
-// const maxOfTwo = (a, b) => {
-//   return a>b ? a : b
-// }
-// console.log(maxOfTwo(129, 251))
+const maxOfTwo = (a, b) => {
+  return a>b ? a : b
+}
+console.log(maxOfTwo(129, 251))
 
 /* 4.
 Write a program to find the maximum out of three given numbers.
 The numbers are num1=8, num2=23 and num3=17.*/
-// const maxOfThree = (a, b, c) => {
-//   return a>b && a>c ? 
-//     a : b>c && b>a ?
-//       b : c
-// }
-// console.log(maxOfThree(8, 23, 17))
+const maxOfThree = (a, b, c) => {
+  return a>b && a>c ? 
+    a : b>c && b>a ?
+      b : c
+}
+console.log(maxOfThree(8, 23, 17))
 
 /* 5.
 Write a program to find the minimum out of three given numbers.
 The numbers are num1=35, num2=29 and num3=46.*/
-// const minOfThree = (a, b, c) => {
-//   return a<b && a<c ? 
-//     a : b<c && b<a ?
-//       b : c
-// }
-// console.log(minOfThree(35, 29, 46))
+const minOfThree = (a, b, c) => {
+  return a<b && a<c ? 
+    a : b<c && b<a ?
+      b : c
+}
+console.log(minOfThree(35, 29, 46))
 
 /*6.
 Write program to take a month as an input from the user and 
 find out whether the month has 31 days or not.*/
-// const takeUSerInp = () => {
-//   // // returns string as 19/05/3214
-//   let readlineSync = require('readline-sync');
-//   return userDate = readlineSync.question('Enter a B\'day Date as [dd/mm/yy]: ');
-// }
-// const daysInMonth = (userDate) => {
-//   return userDate.split('/')[1] in [1, 3, 5, 7, 8, 10, 12] ?
-//     "Month has 31 days" : "Month does not have 31 days"
-// }
-// console.log(daysInMonth(takeUSerInp()))
+const takeUSerInp = () => {
+  // // returns string as 19/05/3214
+  let readlineSync = require('readline-sync');
+  return userDate = readlineSync.question('Enter a B\'day Date as [dd/mm/yy]: ');
+}
+const daysInMonth = (userDate) => {
+  return userDate.split('/')[1] in [1, 3, 5, 7, 8, 10, 12] ?
+    "Month has 31 days" : "Month does not have 31 days"
+}
+console.log(daysInMonth(takeUSerInp()))
--- build-logic-solutions/001_operators_branching_loops/intermediate.js
+++ build-logic-solutions/001_operators_branching_loops/intermediate.js
@@ -7,20 +7,20 @@ for every multiples of 3 & 5, replace with "FizzBuzz".
 Your output should look something like this:
 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17 .....
 */
-// // ES6 shorthand to create an array of 0 to N integers
-// // [...Array(100).keys()]
-// const fizzBuzz = (length) => {
-//   let arr = [];
-//   for (let i=1; i<=length; i++) {
-//     // three condition evaluation
-//     i%3===0 && i%5===0 ? 
-//       arr.push("FizzBuzz") : i%3===0 ?
-//         arr.push("Fizz") : i%5===0 ?
-//           arr.push("Buzz") : arr.push(i)
-//   }
-//   return arr;
-// }
-// console.log(fizzBuzz(100))
+// ES6 shorthand to create an array of 0 to N integers
+// [...Array(100).keys()]
+const fizzBuzz = (length) => {
+  let arr = [];
+  for (let i=1; i<=length; i++) {
+    // three condition evaluation
+    i%3===0 && i%5===0 ? 
+      arr.push("FizzBuzz") : i%3===0 ?
+        arr.push("Fizz") : i%5===0 ?
+          arr.push("Buzz") : arr.push(i)
+  }
+  return arr;
+}
+console.log(fizzBuzz(100))
 
 
 /*2. 
@@ -31,38 +31,38 @@ Print the following star pattern :-
 * * * * 
 * * * * *
 */
-// const printStars = (lines) => {
-//   // long string like "*\n* **\n"
-//   let finalStr = ""   
-//   for (let i=1; i<=lines; i++) {  
-//     // stars on each line "***"
-//     let stars = ""  
-//     for (let j=1; j<=i; j ++) {
-//       stars+="*"
-//     }
-//     // console.log(stars)
-//     finalStr+=stars+'\n'
-//   }
-//   return finalStr
-// }
-// console.log(printStars(5))
+const printStars = (lines) => {
+  // long string like "*\n* **\n"
+  let finalStr = ""   
+  for (let i=1; i<=lines; i++) {  
+    // stars on each line "***"
+    let stars = ""  
+    for (let j=1; j<=i; j ++) {
+      stars+="*"
+    }
+    // console.log(stars)
+    finalStr+=stars+'\n'
+  }
+  return finalStr
+}
+console.log(printStars(5))
 
 
 /*3. 
 Write a program to take a number input from user and 
 print multiplication table 12 times for that number.*/
-// const userInp = () => {
-//   let readlineSync = require('readline-sync');
-//   return userNum = readlineSync.question('Enter a Number: ');
-// }
-// const printMathTable = (userNum) => {
-//   let finalStr = `Multiplication Table for ${userNum} \n`
-//   for (let i=1; i<=12; i++ ) {
-//     finalStr+=`\t${userNum} x ${i} = ${userNum*i}\n`
-//   }
-//   return finalStr
-// }
-// printMathTable(userInp())
+const userInp = () => {
+  let readlineSync = require('readline-sync');
+  return userNum = readlineSync.question('Enter a Number: ');
+}
+const printMathTable = (userNum) => {
+  let finalStr = `Multiplication Table for ${userNum} \n`
+  for (let i=1; i<=12; i++ ) {
+    finalStr+=`\t${userNum} x ${i} = ${userNum*i}\n`
+  }
+  return finalStr
+}
+printMathTable(userInp())
 
 /*4.
 Write a program to return a Fibonacci series : 0,1,1,2,3,5,8,13,21....*/
@@ -86,21 +86,21 @@ Write a program to return a Fibonacci series : 0,1,1,2,3,5,8,13,21....*/
 /*5.
 Write a program to take an input from a user and 
 find its Factorial. Example: Factorial of 5 is 120*/
-// const userInp = () => {
-//   let readlineSync = require('readline-sync');
-//   return userNum = readlineSync.question('Enter a Number: ');
-// }
-// const printFactorial = (userNum) => {
-//   let reducer = userNum;
-//   let factorial = 1;
-//   while (reducer >=1 ) {
-//     // console.log(reducer)
-//     factorial *= reducer
-//     reducer-=1;
-//   }
-//   return `Factorial of ${userNum} is ${factorial}`
-// }
-// console.log(printFactorial(userInp()))
+const userInp = () => {
+  let readlineSync = require('readline-sync');
+  return userNum = readlineSync.question('Enter a Number: ');
+}
+const printFactorial = (userNum) => {
+  let reducer = userNum;
+  let factorial = 1;
+  while (reducer >=1 ) {
+    // console.log(reducer)
+    factorial *= reducer
+    reducer-=1;
+  }
+  return `Factorial of ${userNum} is ${factorial}`
+}
+console.log(printFactorial(userInp()))
 
 
 /*6.
@@ -115,23 +115,23 @@ find if the number is Prime or not.*/
 Write a program to take a day as an input and 
 determine whether it is a weekday or weekend. 
 Example: Tuesday is weekday. */
-// const userInp = () => {
-//   let readlineSync = require('readline-sync');
-//   return userDay = readlineSync.question('Enter a day: ');
-// }
-// const isWeekDay = (userDay) => {
-//   const WEEK = {
-//     1: 'monday',
-//     2: 'tuesday',
-//     3: 'wednesday',
-//     4: 'thursday',
-//     5: 'friday',
-//     6: 'saturday',
-//     7: 'sunday',
-//   }
-//   for (let i in WEEK) {
-//     return userDay.toLowerCase() === WEEK[i] && i>=1 && i<=5 ? 
-//       `${userDay} is Weekday` : `${userDay} is Weekend`
-//   }
-// }
-// console.log(isWeekDay(userInp()))
+const userInp = () => {
+  let readlineSync = require('readline-sync');
+  return userDay = readlineSync.question('Enter a day: ');
+}
+const isWeekDay = (userDay) => {
+  const WEEK = {
+    1: 'monday',
+    2: 'tuesday',
+    3: 'wednesday',
+    4: 'thursday',
+    5: 'friday',
+    6: 'saturday',
+    7: 'sunday',
+  }
+  for (let i in WEEK) {
+    return userDay.toLowerCase() === WEEK[i] && i>=1 && i<=5 ? 
+      `${userDay} is Weekday` : `${userDay} is Weekend`
+  }
+}
+console.log(isWeekDay(userInp()))
