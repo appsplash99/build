3f6c48425f48dcf5ffd5925aad11c95ae72c19ae
appsplash99 <apurvchimralwar1997@gmail.com>
2021-02-25 22:21:18 +0530

    complete last exercise and confirm others

---
+++ build-logic-solutions/002_functions/easy.js
@@ -0,0 +1,78 @@
+/*1.
+Given a and b, your function should return the value of a^b
+Example:
+Input: power(2,3) ––> Output: 8
+*/
+// const power = (a, b) => {
+//   return a**b
+// }
+// console.log(power(2, 3))
+
+
+/*2.
+Given length of a regular hexagon, your function should return area of the hexagon.
+Example:
+Input: areaOfHexagon(10) ––> Output: 259.80
+*/
+// const areaOfHexagon = (sides) => {
+//   return (3*(3**(1/2))*(sides**2)/2).toFixed(2)
+// }
+// console.log(areaOfHexagon(10))
+// ##########################################################
+// #### TODO: CONFIRM if OUTPUT 259.81 is correct or NOT? ###
+// ##########################################################
+
+
+
+/*3.
+Given a sentence, your functions should return the number of words in the sentence.
+Example:
+Input: noOfWords(We are neoGrammers) ––> Output: 3*/
+// const noOfWords = (str) => {
+//   let count = 0;
+//   for (let i=0; i<str.length; i++) {
+//     if (str[i]===' ') {
+//       count++;
+//     }
+//   }
+//   return count+1
+// }
+// console.log(noOfWords("We are neoGrammers"))
+
+
+/*4.
+Given n numbers, your function should return the minimum of them all. The number of parameters won't be accepted from user.
+Example:
+Input: findMin(3,5) ––> Output: 3
+Input: findMin(3,5,9,1) ––> Output: 1
+(Hint: Lookup rest parameters in JavaScript)*/
+// const findMin = (...args) => {
+//   return Math.min(...args)
+// }
+// console.log(findMin(3,5,9,1))
+// ################################
+// #### TODO: CONFIRM if Valid? ###
+// ################################
+
+
+/*5.
+Given n numbers, your function should return the maximum of them all. The number of parameters won't be accepted from user.
+Example:
+Input: findMax(3,5) ––> Output: 5
+Input: findMax(3,5,9,1) ––> Output: 9
+(Hint: Lookup rest parameters in JavaScript)*/
+// const findMax = (...args) => {
+//   return Math.max(...args)
+// }
+// console.log(findMax(3,5,9,1))
+
+
+/*6.
+Given three angles of a triange, 
+your function should return if it is a scalen, isosceles, equilateral triangle or not a triangle at all. Example:
+Input: typeOfTriangle(30, 60, 90) ––> Output: Scalen Triangle*/
+// ####################
+// #### TODO: #########
+// ####################
+
+
